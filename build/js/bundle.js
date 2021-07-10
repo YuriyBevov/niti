06a98486386687d1a2495bed881065836215b4d3
Yuriy Bevov <jury19y@mail.ru>
2021-07-10 19:40:46 +0300

    почистил js

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -12284,16 +12284,12 @@ window.addEventListener('resize', onWindowResizeHandler)
 
 
 const overlay = document.querySelector('.contacts__overlay')
-console.log(overlay)
 
 const onClickRemoveOverlay = () => {
     overlay.style.display = 'none'
 }
 
 overlay.addEventListener('click', onClickRemoveOverlay)
-
-
-console.log('overlay')
 
 /***/ }),
 
