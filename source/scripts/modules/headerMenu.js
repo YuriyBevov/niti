87ea060d96cf2e0669e5133017efd80c07811f78
Yuriy Bevov <jury19y@mail.ru>
2021-07-18 18:18:47 +0300

    бургер

--- source/scripts/modules/headerMenu.js
+++ source/scripts/modules/headerMenu.js
@@ -3,7 +3,14 @@ const menu = document.querySelector('.nav');
 const header = document.querySelector('.header');
 
 const onClickOpenMenu = () => {
-    burger.classList.toggle('opened')
+    if(burger.classList.contains('opened')) {
+        burger.classList.remove('opened')
+        burger.classList.add('closed')
+    } else {
+        burger.classList.remove('closed')
+        burger.classList.add('opened')
+    }
+    
     header.classList.toggle('js-mobile-opened')
 };
 
@@ -12,6 +19,9 @@ burger.addEventListener('click', onClickOpenMenu);
 window.addEventListener('resize', () => {
     if(window.innerWidth > 767 && header.classList.contains('js-mobile-opened')) {
         header.classList.remove('js-mobile-opened');
-        burger.classList.remove('opened')
+        if(burger.classList.contains('opened')) {
+            burger.classList.remove('opened')
+            burger.classList.add('closed')
+        }
     }
 })
