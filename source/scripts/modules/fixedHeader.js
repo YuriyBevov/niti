71b9a34b4c1494246cead02e4994b381c8e7fa7e
Yuriy Bevov <jury19y@mail.ru>
2021-07-18 16:45:36 +0300

    убрал алерты

--- source/scripts/modules/fixedHeader.js
+++ source/scripts/modules/fixedHeader.js
@@ -10,11 +10,7 @@ main.style.marginTop = `${headerInitHeight}px`;
 intro.style.height = `${window.innerHeight - headerInitHeight}px`;
 
 const setIntroBlockHeight = () => {
-    console.log('func')
-    
-
     const setHeight = (height) => {
-        console.log('setHeight')
         intro.style.height = `${height - headerInitHeight}px`;
     }
 
@@ -22,9 +18,7 @@ const setIntroBlockHeight = () => {
     const width = window.innerWidth;
 
     const checkHeaderHeightChange = () => {
-
         if(headerInitHeight !== header.offsetHeight) {
-            console.log('change header')
             headerInitHeight = header.offsetHeight
             main.style.marginTop = `${header.offsetHeight}px`;
         }
@@ -33,24 +27,16 @@ const setIntroBlockHeight = () => {
     checkHeaderHeightChange();
 
     if( height < 534 ) {
-        console.log('< 534')
-        alert('< 534')
-
         checkHeaderHeightChange();
         setHeight(534);
     } 
     
     if (height < 661 && width > 1140) {
-        console.log('< 661')
-        alert('< 661 && width > 1140')
-
         checkHeaderHeightChange();
         setHeight(700);
     }
 
     if(height > 1640) {
-        alert('> 1640')
-        console.log('>')
         checkHeaderHeightChange();
         setHeight(960);
     }
