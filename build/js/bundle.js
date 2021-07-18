725e5ebf0941ae92744561b8c1be5a1e4fcada05
Yuriy Bevov <jury19y@mail.ru>
2021-07-18 13:37:33 +0300

    проверка высоты

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -17821,16 +17821,20 @@ const main = document.querySelector('main');
 const intro = document.querySelector('.intro');
 console.log(intro.offsetHeight)
 
-main.style.marginTop = `${header.offsetHeight}px`;
+let headerInitHeight = header.offsetHeight;
+
+main.style.marginTop = `${headerInitHeight}px`;
 
 header.style.position = 'fixed';
 
 intro.style.height = `${intro.offsetHeight - header.offsetHeight}px`;
 
 window.addEventListener('resize', () => {
-    const header = document.querySelector('.header');
-    main.style.marginTop = `${header.offsetHeight}px`;
-    
+    if(headerInitHeight !== header.offsetHeight) {
+        headerInitHeight = header.offsetHeight;
+        main.style.marginTop = `${headerInitHeight}px`;
+        console.log('change')
+    }
 })
 
 /***/ }),
